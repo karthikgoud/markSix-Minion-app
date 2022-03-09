18f12417b01f8bb49f3258dacc1be82cea7f0b3e
karthikgoud <karthikrv09@gmail.com>
2022-03-09 16:09:09 +0530

    wiring button

--- minion-app.js
+++ minion-app.js
@@ -1,7 +1,12 @@
+
+// reference button s=using querySelector
 var buttonClick = document.querySelector("#btnTranslate");
 
+// add eventlistener
 buttonClick.addEventListener("click",run);
 
+// function for eventListner
 function run(){
     console.log("clicked");
+
 }
